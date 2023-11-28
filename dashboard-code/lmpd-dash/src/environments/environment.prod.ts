6da34cec263486fffb4a0e41557b5ddc99e0772c
colorscheme-xik3 <francisco.d.clr@gmail.com>
2023-11-28 13:58:40 +0000

    adding angular to dashboard

---
+++ dashboard-code/lmpd-dash/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'limpid-7f8dc',
+    appId: '1:912014708375:web:20593ded086b3b0fdd63d2',
+    databaseURL: 'https://limpid-7f8dc-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'limpid-7f8dc.appspot.com',
+    apiKey: 'AIzaSyCdLQMTIQB0EBZ-Mh6wGT-7WjvrcrMcep0',
+    authDomain: 'limpid-7f8dc.firebaseapp.com',
+    messagingSenderId: '912014708375',
+    measurementId: 'G-75HRBZ712P',
+  },
+  production: true
+};
---
+++ dashboard-code/lmpd-dash/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'limpid-7f8dc',
+    appId: '1:912014708375:web:20593ded086b3b0fdd63d2',
+    databaseURL: 'https://limpid-7f8dc-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'limpid-7f8dc.appspot.com',
+    apiKey: 'AIzaSyCdLQMTIQB0EBZ-Mh6wGT-7WjvrcrMcep0',
+    authDomain: 'limpid-7f8dc.firebaseapp.com',
+    messagingSenderId: '912014708375',
+    measurementId: 'G-75HRBZ712P',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
